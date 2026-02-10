4c7a84eeb855e3d52ed0dccc5cc89df7bfe5ada6
Denelia <janinedenelia7@gmail.com>
2026-02-10 23:09:15 +0200

    added js poem generator function and added typewriter pluggin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:"Ek was gevra om ietsie oor Pa te sê",
+    autoStart:true,
+    delay:1,
+    cursor:"",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
